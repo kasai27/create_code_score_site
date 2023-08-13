e7cc5b53214d10452109301d4e1800ccb6fb4b5d
kasai <back2846@icloud.com>
2023-08-13 15:45:11 +0900

    入力に対しての動作を追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
+// import Code from "./components/Code";
 
 function App() {
+  // 入力される状態管理
+  const [title, setTitle] = useState(null);
+  const [name, setName] = useState(null);
+  const [kapo, setKapo] = useState(null);
+  const [key, setKey] = useState(null);
+  const [liric, setLiric] = useState(null);
+
+  // キーの入力に対しての返答の状態管理
+  const [response, setResponse] = useState('');
+
+  // キーの入力に対してダイアトニックコードのリスト管理
+  const [keyList, setKeyList] = useState([]);
+
+  // それぞれ入力された時の関数
+  const onChangeTitle = (e) => setTitle(e.target.value);
+  const onChangeName = (e) => setName(e.target.value);
+  const onChangeKapo = (e) => setKapo(e.target.value);
+  const onChangeKey = (e) => {
+    setKey(e.target.value);
+    // ここで入力に応じた返答を設定するロジックを追加
+    setResponse(generateResponse(e.target.value));
+  }
+  const onChangeLiric = (e) => setLiric(e.target.value);
+
+  // const crateKeyList = (key) => {
+      // if (key == "C" ||  key == "Am") {
+          // setKeyList(["C", "Dm", "Em", "F", "G", "Am", "Bm"])
+      // }
+  // }
+  
+  // 入力に応じた返答を生成するロジック
+  const generateResponse = (input) => {
+    if (input == "C" || input == "Am") {
+      setKeyList(["C", "Dm", "Em", "F", "G", "Am", "Bm"])
+    } else if (input == "C#" || input == "A#m") {
+      setKeyList(["C#", "D#m", "Fm", "F#", "G#", "A#m", "Cm"])
+    } else if (input == "D" || input == "Bm") {
+      setKeyList(["D", "Em", "F#m", "G", "A", "Bm", "C#m"])
+    } else if (input == "E♭" || input == "Cm") {
+      setKeyList(["E♭", "Fm", "Gm", "A♭", "B♭", "Cm", "Dm"])
+    } else if (input == "E" || input == "C#m") {
+      setKeyList(["E", "F#m", "G#m", "A", "B", "C#m", "D#m"])
+    } else if (input == "F" || input == "Dm") {
+      setKeyList(["F", "Gm", "Am", "B♭", "C", "Dm", "Em"])
+    } else if (input == "F#" || input == "D#m") {
+      setKeyList(["F#", "G#m", "A#m", "B", "C#", "D#m", "Fm"])
+    } else if (input == "G" || input == "Em") {
+      setKeyList(["G", "Am", "Bm", "C", "D", "Em", "F#m"])
+    } else if (input == "A♭" || input == "Fm") {
+      setKeyList(["A♭", "B♭m", "Cm", "D♭", "E♭", "Fm", "Gm"])
+    } else if (input == "A" || input == "F#m") {
+      setKeyList(["A", "Bm", "C#m", "D", "E", "F#m", "G#m"])
+    } else if (input == "B♭" || input == "Gm") {
+      setKeyList(["B♭", "Cm", "Dm", "E♭", "F", "Gm", "Am"])
+    } else if (input == "B" || input == "G#m") {
+      setKeyList(["B", "C#m", "D#m", "E", "F#", "G#m", "A#m"])
+    }
+  };
+
+  // コードのボタンが押された時のロジック
+  const onClickCodeButton = (code) => {
+    setLiric((prevContent) => prevContent + code)
+  }
+
   return (
     <div className="App">
       <h1>楽譜作成</h1>
+
       <form>
         <ul>
           <li>
-            <input type="text" placeholder="曲名"/>
+            <input type="text" placeholder="曲名" onChange={onChangeTitle} />
           </li>
           <li>
-            <input type="text" placeholder="アーティスト名"/>
+            <input type="text" placeholder="アーティスト名" onChange={onChangeName} />
           </li>
           <li>
-            <input type="text" placeholder="キー"/>
+            <input type="text" placeholder="カポ" onChange={onChangeKapo} />
           </li>
           <li>
-            <input type="text" placeholder="カポ"/>
+            <input type="text" value={key} placeholder="キー" onChange={onChangeKey} />
           </li>
         </ul>
-        <input type="text" placeholder="歌詞，コード" />
       </form>
-      
-      
-      <button type="submit">作成</button>
+          
+      <form>
+        <button onClick={(e) => {e.preventDefault(); onClickCodeButton(keyList[0])}}>{keyList[0]}</button>
+        <button onClick={(e) => {e.preventDefault(); onClickCodeButton(keyList[1])}}>{keyList[1]}</button>
+        <button onClick={(e) => {e.preventDefault(); onClickCodeButton(keyList[2])}}>{keyList[2]}</button>
+        <button onClick={(e) => {e.preventDefault(); onClickCodeButton(keyList[3])}}>{keyList[3]}</button>
+        <button onClick={(e) => {e.preventDefault(); onClickCodeButton(keyList[4])}}>{keyList[4]}</button>
+        <button onClick={(e) => {e.preventDefault(); onClickCodeButton(keyList[5])}}>{keyList[5]}</button>
+        <button onClick={(e) => {e.preventDefault(); onClickCodeButton(keyList[6])}}>{keyList[6]}</button>
+      </form>
+      <input type="text" value={liric} onChange={onChangeLiric} placeholder="歌詞，コード" />
+      <button type="submit" >作成</button>
+
     </div>
   );
-}
+};
 
 export default App;
